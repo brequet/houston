82d30ddbc8efa1861fbb5b9344721a9b9a01f946
Baptiste REQUET <baptiste.requet@ext.biomerieux.com>
2024-01-19 16:38:35 +0100

    navbar + pages

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,22 +1,58 @@
 <script lang="ts">
-  import Router from "svelte-spa-router";
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "./assets/vite.svg";
-  import Counter from "./lib/Counter.svelte";
-  import A from "./routes/A.svelte";
-  import B from "./routes/B.svelte";
-  import Button from "$lib/components/ui/button/button.svelte";
+  import { Button } from "$lib/components/ui/button";
+  import { LayoutDashboard, Terminal } from "lucide-svelte";
+  import type { ComponentType } from "svelte";
+  import Router, { location } from "svelte-spa-router";
+  import DashboardPage from "./routes/DashboardPage.svelte";
+  import ScriptsPage from "./routes/ScriptsPage.svelte";
 
-  const routes = {
-    "/a": A,
-    "/b": B,
-  };
+  const navItems: {
+    name: string;
+    path: string;
+    component: ComponentType;
+    icon: ComponentType;
+  }[] = [
+    {
+      name: "Dashboard",
+      path: "/dashboard",
+      component: DashboardPage,
+      icon: LayoutDashboard,
+    },
+    {
+      name: "Scripts",
+      path: "/script",
+      component: ScriptsPage,
+      icon: Terminal,
+    },
+  ];
+
+  const routes = navItems.reduce(
+    (accumulator, navItem) => {
+      accumulator[navItem.path] = navItem.component;
+      return accumulator;
+    },
+    {} as Record<string, ComponentType>,
+  );
 </script>
 
-Root
+<div class="w-screen h-screen flex flex-row">
+  <nav class="w-60 border-r py-2 px-2">
+    <h2 class="text-xl font-semibold py-2 px-4 tracking-tight">Overview</h2>
+    <div class="grid items-start gap-2">
+      {#each navItems as navItem}
+        <Button
+          href="#{navItem.path}"
+          class="flex-grow justify-start"
+          variant={$location === navItem.path ? "secondary" : "ghost"}
+        >
+          <svelte:component this={navItem.icon} class="mr-2 w-4 h-4" />
+          {navItem.name}
+        </Button>
+      {/each}
+    </div>
+  </nav>
 
-<Button href="#/">Go root</Button>
-<Button href="#/a">Go to A</Button>
-<Button href="#/b">Go to A</Button>
-
-<Router {routes} />
+  <main class="flex-grow m-4">
+    <Router {routes} />
+  </main>
+</div>
